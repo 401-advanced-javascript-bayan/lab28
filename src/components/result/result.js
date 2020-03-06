a8a9f4bee03b023718f58ba3b12d0e54c79209f7
bayan <bayana.mhmd99@gmail.com>
2020-03-07 01:16:07 +0200

    result

---
+++ src/components/result/result.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactJson from  'react-json-view'
+
+function Result (props){
+    return(
+        <ReactJson src={props.result} />
+    );
+}
+
+export default Result ;
